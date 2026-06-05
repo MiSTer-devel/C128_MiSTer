f3bb49fa5477e742ec5d86bb0dd13b801d3d7f85
Roberto Lari <lroby74@gmail.com>
2026-06-05 22:00:40 +0200

    Add Snac support for DB9 joysticks with autofire

--- c128.sv
+++ c128.sv
@@ -301,6 +301,8 @@ localparam CONF_STR = {
    "P2FC5,CRT,Boot Cartridge              ;",
    "-;",
    "O[3],Swap Joysticks,No,Yes;",
+   "O[104:103],SNAC Joystick,Disabled,Joy 1,Joy 2,Joy 1 + 2;",
+   "O[88:87],SNAC Autofire,Off,Slow,Fast;",
    "-;",
    "O[49:48],8502 Speed,Standard,x2,x3,x4;",
    "HCO[101],Z80 Speed,Standard,x2;",
@@ -445,6 +447,21 @@ end
 
 wire  [15:0] joyA,joyB,joyC,joyD;
 wire  [15:0] joy = joyA | joyB | joyC | joyD;
+wire   [1:0] snac_mode = status[104:103]; // 0=disabled, 1=Joy1, 2=Joy2, 3=Joy+Joy2
+wire         snac_en   = |snac_mode;
+// snac_joy: {fire3=0, fire2, fire1, right, left, down, up} - active high
+wire [6:0] snac_joy  = {1'b0, ~USER_IN[6], ~USER_IN[2], ~USER_IN[3], ~USER_IN[5], ~USER_IN[0], ~USER_IN[1]};
+
+// format: {fire3=0, fireB(Pin9), fireA(Pin6), right(Pin4), left(Pin3), down(Pin2), up(Pin1)}
+
+// SNAC Autofire: oscillatore software, non richiede il +5V hardware (DB9 Pin 5)
+// Fire 1 (DB9 Pin 6) viene modulato; Fire 2 (DB9 Pin 9) passa sempre diretto
+wire [1:0] snac_af_mode = status[88:87];
+reg [21:0] snac_af_cnt;
+always @(posedge clk_sys) snac_af_cnt <= snac_af_cnt + 1'd1;
+// Slow ~7.6 Hz | Fast ~15.3 Hz  (clk_sys = 32 MHz)
+wire snac_af_clk = (snac_af_mode == 2'd2) ? snac_af_cnt[20] : snac_af_cnt[21];
+wire [6:0] snac_joy_af = {snac_joy[6:5], snac_af_mode ? (snac_joy[4] & snac_af_clk) : snac_joy[4], snac_joy[3:0]};
 
 reg          status_set;
 reg          status_in_98;
@@ -793,8 +810,10 @@ wire [6:0] joyC_c64 = joy[9:8] ? 7'd0 : {joyC[6:4], joyC[0], joyC[1], joyC[2], j
 wire [6:0] joyD_c64 = joy[9:8] ? 7'd0 : {joyD[6:4], joyD[0], joyD[1], joyD[2], joyD[3]};
 
 // swap joysticks if requested
-wire [6:0] joyA_c64 = status[3] ? joyB_int : joyA_int;
-wire [6:0] joyB_c64 = status[3] ? joyA_int : joyB_int;
+wire [6:0] joyA_base = status[3] ? joyB_int : joyA_int;
+wire [6:0] joyB_base = status[3] ? joyA_int : joyB_int;
+wire [6:0] joyA_c64  = (snac_mode == 2'd1 || snac_mode == 2'd3) ? snac_joy_af : joyA_base;
+wire [6:0] joyB_c64  = (snac_mode == 2'd2 || snac_mode == 2'd3) ? snac_joy_af : joyB_base;
 
 wire [7:0] paddle_1 = status[3] ? pd3 : pd1;
 wire [7:0] paddle_2 = status[3] ? pd4 : pd2;
@@ -1728,50 +1747,19 @@ end
 
 wire ext_iec_en = status[25];
 
-iec_io iec_io_clk
-(
-   .clk(clk_sys),
-   .ext_en(ext_iec_en),
+wire ext_iec_clk  = |snac_mode ? 1'b1 : (USER_IN[2] | ~ext_iec_en);
+wire ext_iec_data = |snac_mode ? 1'b1 : (USER_IN[4] | ~ext_iec_en);
+wire ext_iec_srq_n = |snac_mode ? 1'b1 : (USER_IN[6] | ~ext_iec_en);
 
-   .cpu_o(c64_iec_clk_o),
-   .drive_o(drive_iec_clk_o),
-   .ext_o(USER_IN[2]),
+assign c64_iec_clk_i = ext_iec_clk;
+assign c64_iec_data_i = ext_iec_data;
+assign c64_iec_srq_n_i = ext_iec_srq_n;
 
-   .cpu_i(c64_iec_clk_i),
-   .drive_i(drive_iec_clk_i),
-   .ext_i(USER_OUT[2])
-);
-
-iec_io iec_io_data
-(
-   .clk(clk_sys),
-   .ext_en(ext_iec_en),
-
-   .cpu_o(c64_iec_data_o),
-   .drive_o(drive_iec_data_o),
-   .ext_o(USER_IN[4]),
-
-   .cpu_i(c64_iec_data_i),
-   .drive_i(drive_iec_data_i),
-   .ext_i(USER_OUT[4])
-);
-
-iec_io iec_io_srq_n
-(
-   .clk(clk_sys),
-   .ext_en(ext_iec_en),
-
-   .cpu_o(c64_iec_srq_n_o),
-   .drive_o(drive_iec_srq_n_o),
-   .ext_o(USER_IN[6]),
-
-   .cpu_i(c64_iec_srq_n_i),
-   .drive_i(drive_iec_srq_n_i),
-   .ext_i(USER_OUT[6])
-);
-
-assign USER_OUT[3] = (reset_n & ~status[6]) | ~ext_iec_en;
-assign USER_OUT[5] = c64_iec_atn | ~ext_iec_en;
+assign USER_OUT[2] = |snac_mode ? 1'b1 : ((c64_iec_clk_o & drive_iec_clk_o) | ~ext_iec_en);
+assign USER_OUT[3] = |snac_mode ? 1'b1 : ((reset_n & ~status[6]) | ~ext_iec_en);
+assign USER_OUT[4] = |snac_mode ? 1'b1 : ((c64_iec_data_o & drive_iec_data_o) | ~ext_iec_en);
+assign USER_OUT[5] = |snac_mode ? 1'b1 : (c64_iec_atn | ~ext_iec_en);
+assign USER_OUT[6] = 1'b1;
 
 wire hblank;
 wire vblank;
